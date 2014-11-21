cd2c9ebf4899651af03ee0b0f1af52684d1e6a12
Fabiano Almeida <fabiano@fabiano-Aspire-4732Z.(none)>
2014-11-20 23:20:22 -0200

    Programming Assignment 2, it's ok

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,5 @@
-## > matrix
-##      [,1] [,2]
-## [1,]    2    3
-## [2,]    2    5
-
-##> det(matrix)
-## 4
-
-##> solve(matrix) %*%  matrix
-##       [,1] [,2]
-##  [1,]   1    0
-##  [2,]   0    1
-
-
 ## This function creates a special
 ## "matrix" object that can cache its inverse.
-
 makeCacheMatrix <- function(x = matrix()) {
             m<-NULL
             set<-function(y){
@@ -22,7 +7,6 @@ makeCacheMatrix <- function(x = matrix()) {
                 m<<-NULL
             }
             get<-function() x
-
             set_matrix<-function(solve) m<<- solve
             get_matrix<-function() m
             list(
@@ -33,10 +17,8 @@ makeCacheMatrix <- function(x = matrix()) {
                 )
 }
 
-
 ## This function computes the inverse of the
 ## special "matrix" returned by makeCacheMatrix above.
-
 cacheSolve <- function(x=matrix(), ...) {
     m<-x$get_matrix()
     if(!is.null(m)){
@@ -46,7 +28,36 @@ cacheSolve <- function(x=matrix(), ...) {
     matrix<-x$get()
     m<-solve(matrix, ...)
     x$set_matrix(m)
-
     m
 }
+### to test uncomment the next line, and run cachematrix.R
+##cacheSolve(makeCacheMatrix(matrix(c(2 , 2, 3, 5), 2, 2)))
 
+## > m <- matrix(c(2 , 2, 3, 5), 2, 2)
+## > m
+##      [,1] [,2]
+## [1,]    2    3
+## [2,]    2    5
+
+##> det(m)
+## 4
+#### Ok, determinat > 0
+### if det(m) == 0 , not Ok
+
+##> solve(m) %*%  matrix
+##       [,1] [,2]
+##  [1,]   1    0
+##  [2,]   0    1
+### identity
+
+## > m_inverse <- solve(m)
+## > m_inverse
+##       [,1]  [,2]
+## [1,]  1.25 -0.75
+## [2,] -0.50  0.50
+
+### To prove
+## m%*%m_inverse
+##      [,1] [,2]
+## [1,]    1    0
+## [2,]    0    1
